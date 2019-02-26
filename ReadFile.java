b70a42e5d4fc290b46bf3f5137cfde67fa89286e
Brandon <bchen00@stuy.edu>
2019-02-26 10:15:46 -0500

    working on adding each character into an array and printing it

---
+++ ReadFile.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class ReadFile {
+	private File text = new File("Maze1.txt");
+	private Scanner inf = new Scanner(text);
+	private int row = 0; 
+	private int col = 0;
+	private char ary[][]; //= new Char[][];
+	
+	public ReadFile(){
+		while(inf.hasNextLine()){
+			row++;
+		}
+		col = inf.nextLine().length();
+		ary = new char[row][col];
+		
+	}
+	public void fillAry(){
+		int currentRow = 0;
+		int currentCol = 0;
+		while(inf.hasNextLine()){
+          	  	String line = inf.nextLine();
+          	  	//System.out.println(line);//hopefully you can do other things with the line
+			for (int i = 0; i < line.length();i++){
+				ary[currentRow][currentCol] = line.charAt(i);
+			}
+			currentRow++;
+			currentCol = 0;
+		}	
+        }      
+	
+	public String toString(){
+		String ans = "";
+		for (int r = 0; r<ary.length;r++){
+			for (int c = 0; c < ary[r].length;c++){
+				ans += ary[r][c];
+			}
+			ans += "\n";
+		}
+		return ans;
+	}
+  public static void main(String args[]) throws FileNotFoundException {
+	ReadFile test1 = new ReadFile();
+	System.out.println(toString());
+        //instead of a try/catch, you can throw the FileNotFoundException.
+
+        //This is generally bad behavior
+ 
+
+        File text = new File("Maze1.txt");
+        // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
+        
+
+        //inf stands for the input file
+
+        Scanner inf = new Scanner(text);
+
+        while(inf.hasNextLine()){
+            String line = inf.nextLine();
+            System.out.println(line);//hopefully you can do other things with the line
+        }       
+    }   
+}
